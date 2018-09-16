78805959e24504670dcdd158e9ce0e3676b9cc18
romanbestaev <romanbestaev@yandex.ru>
2018-09-16 22:47:59 +0300

    new

--- RemoveDuplicates.py
+++ RemoveDuplicates.py
@@ -1,21 +1,23 @@
-def removeDuplicates(self, nums):
-    """
-    :type nums: List[int]
-    :rtype: int
-    """
-    if nums:
-        len_ = 1
-        flag = 1
-    else:
-        len_ = 0
-        flag = 0
-    while flag:
-        if len(nums)>len_:
-            if nums[len_]==nums[len_-1]:
-                nums.pop(len_)
-            else:
-                len_ += 1
+class Solution:
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if nums:
+            len_ = 1
+            flag = 1
         else:
+            len_ = 0
             flag = 0
+        while flag:
+            if len(nums)>len_:
+                if nums[len_]==nums[len_-1]:
+                    nums.pop(len_)
+                else:
+                    len_ += 1
+            else:
+                flag = 0
 
-    return len_
+        return len_
+        
