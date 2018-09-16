1b24a2994679f6132b25a9d1a4734c63a74f99f5
romanbestaev <romanbestaev@yandex.ru>
2018-09-16 22:35:30 +0300

    Initial script_

---
+++ RemoveDuplicates.py
@@ -0,0 +1,21 @@
+def removeDuplicates(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    if nums:
+        len_ = 1
+        flag = 1
+    else:
+        len_ = 0
+        flag = 0
+    while flag:
+        if len(nums)>len_:
+            if nums[len_]==nums[len_-1]:
+                nums.pop(len_)
+            else:
+                len_ += 1
+        else:
+            flag = 0
+
+    return len_
